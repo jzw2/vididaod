18bcaa43233d28076bbb3040e70747471eb97443
John Wang <jzw2@illinois.edu>
2018-11-15 13:58:43 -0600

    Did some stuff to the main memory
    Krist also made me convert spaces to tabs, which is dumb because he should have
    done that is his own vim thing.

--- src/main_memory.v
+++ src/main_memory.v
@@ -1,6 +1,6 @@
 module main_memory(data_out, addr, data_in, write_enable, clk, reset);
 	output	[word_size-1:0] data_out; 
-	input	[len-1:0] addr;
+	input	[len_log_2 - 1:0] addr;
 	input	[word_size-1:0]  data_in;
 	input	write_enable;
 	input	clk;
@@ -9,16 +9,18 @@ module main_memory(data_out, addr, data_in, write_enable, clk, reset);
 	parameter begin
 		word_size = 8;
 		len = 65000;
+    len_log_2 = 16;
+     
 	end
 
-	reg [7:0] data [len-1:0];
+	reg [word_size-1:0] data [0:len-1];
 
 	always @ (posedge clk) begin
 		if (write_enable)
-			data[index] <= data_in;
+			data[addr] <= data_in;
 	end
 		 
-	assign data_out = data[index];
+	assign data_out = data[addr];
 	   
 
 endmodule
