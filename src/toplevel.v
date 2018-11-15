156f452be899c1e2232f12c4cd25871c176b09e4
John Wang <jzw2@illinois.edu>
2018-11-15 14:20:13 -0600

    Added a toplveel, and inculed the basys file

---
+++ src/toplevel.v
@@ -0,0 +1,6 @@
+module toplevel(input btnC, input btnU, input btnL, input btnR, input btnD, input swt, input clk, output y);
+
+   main main_module(btC, btU, btnL, btnR, btnD, swt, clk, ,);
+   //make the output thing 
+
+endmodule; //ligma
