31845abee5404c40cb797097b7307fe8209d76a5
Krist Pregracke <kristbp2@siebl-0222-14.ews.illinois.edu>
2018-11-30 19:24:13 -0600

    fixed johns mistake

--- wave_data_gen/wave_data_gen.py
+++ wave_data_gen/wave_data_gen.py
@@ -1,7 +1,7 @@
 import math
 
 def freq(n):
-	return math.pow(2, (n-49)/12)*440
+  return math.pow(2, (n-49)/12)*440
 
 scale_fac = 65536.0/4 #just choose a nice number, this happens to work quite well
 
@@ -13,25 +13,25 @@ num_bytes = 0
 num_bytes_partial = 0
 for n in range(1, 88, 4): #n is every fourth key 
 
-	note_length = int(round((scale_fac/freq(n))))
-	lengths += '{:x},\n'.format(note_length)
-	off += hex(num_bytes)[2:] + ",\n" #write to the offsets file the offset of this wave
-	for x in range(0, note_length): #map to integers scale with scale_fac * 1/freq(n), then round to get closeset end sample
-		num_bytes_partial += 1 #incrememnt the number of bytes for this single wave
-		t = hex(int(math.floor(128*(math.sin(2*math.pi*freq(n)*x/scale_fac)+1))))[2:] + ", "
-		if(len(t) != 4):
-			t += " "
-		o += t
-		#stuff to make it more human readable, every 40 bytes => new line, every wave => new line
-		if(col >= 40):
-			o += "\n"
-			col = 0
-		col += 1
-		num_bytes += 1 #incrememnt the total number of bytes for all the waves
-	print(num_bytes_partial)
-	num_bytes_partial = 0
-	col = 0
-	o+= "\n\n"
+  note_length = int(round((scale_fac/freq(n))))
+  lengths += hex(note_length)[2:] + ",\n"
+  for x in range(0, note_length): #map to integers scale with scale_fac * 1/freq(n), then round to get closeset end sample
+    num_bytes_partial += 1 #incrememnt the number of bytes for this single wave
+    t = hex(int(math.floor(128*(math.sin(2*math.pi*freq(n)*x/scale_fac)+1))))[2:] + ", "
+    if(len(t) != 4):
+      t += " "
+    o += t
+    #stuff to make it more human readable, every 40 bytes => new line, every wave => new line
+    if(col >= 40):
+      o += "\n"
+      col = 0
+    col += 1
+    num_bytes += 1 #incrememnt the total number of bytes for all the waves
+  off += hex(num_bytes)[2:] + ",\n" #write to the offsets file the offset of this wave
+  print(num_bytes_partial)
+  num_bytes_partial = 0
+  col = 0
+  o+= "\n\n"
 print(num_bytes)
 f = open("sine_waveform.coe", "w")
 o = o.strip()
