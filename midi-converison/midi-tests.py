72d3730457a22f1601887ee8f5aa83f2e1fe4548
John Wang <jzw2@illinois.edu>
2018-12-05 16:45:46 -0600

    did midi conversion

--- midi-converison/midi-tests.py
+++ midi-converison/midi-tests.py
@@ -7,5 +7,19 @@ mid = MidiFile('OnceUponAMIDI.mid')
 #    for msg in track:
 #        print(msg)
 
+converted_notes = []
+SpeedUpFactor = 8
 for msg in mid.tracks[2]:
-	print(msg)
+  for i in range(msg.time / 8):
+    if msg.type == 'note_on':
+      converted_notes.append(msg.note)
+    elif msg.type == 'note_off':
+      converted_notes.append(0)
+
+f = open("song1.coe", "w")
+converted_str = "memory_initialization_radix=16;\nmemory_initialization_vector=\n"
+
+converted_str += ",".join([("%x" % note) for note in converted_notes]) + ";"
+f.write(converted_str)
+f.close 
+print(converted_notes)
